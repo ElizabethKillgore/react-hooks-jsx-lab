d905257f070dcd608328d951dcecb56750a325a3
Elizabeth <ek@elizabethkillgore.com>
2023-12-12 19:34:17 -0500

    jsx lab

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -15,14 +15,14 @@ test("renders the h1 with the text 'Name is a Web Developer from City'", () => {
   // Find an element with the text content `${name} is a Web Developer from ${city}`
   // This uses the variables defined in src/data/data.js
   const h1 = screen.queryByText(`${name} is a Web Developer from ${city}`);
-
+  console.log(name)
   expect(h1).toBeInTheDocument();
   expect(h1.tagName).toBe("H1"); // check that the element is a <h1>
 });
 
 test("the h1 has a an inline style attribute with a color of 'firebrick'", () => {
   render(<Home />);
-
+  
   const h1 = screen.queryByText(`${name} is a Web Developer from ${city}`);
   expect(h1).toHaveStyle({ color: "firebrick" });
 });
